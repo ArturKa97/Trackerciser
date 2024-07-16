d59abeeebbaf03f6f2736515a3a0faed6475db30
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-07-16 21:28:32 +0300

    Added first simple Exercise controller

---
+++ project/backend/src/main/java/com/exercise/project/controllers/ExerciseController.java
@@ -0,0 +1,13 @@
+package com.exercise.project.controllers;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@NoArgsConstructor
+//@AllArgsConstructor
+@RequestMapping("/exercise")
+public class ExerciseController {
+}
