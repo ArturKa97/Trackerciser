61f3afbd112961e27a188a3e8487969a3b79fbf5
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-07-17 20:53:40 +0300

    Implemented simple validation for Exercise entity.

--- project/backend/src/main/java/com/exercise/project/controllers/ExerciseController.java
+++ project/backend/src/main/java/com/exercise/project/controllers/ExerciseController.java
@@ -2,6 +2,7 @@ package com.exercise.project.controllers;
 
 import com.exercise.project.entities.Exercise;
 import com.exercise.project.services.ExerciseService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +16,7 @@ public class ExerciseController {
     private final ExerciseService exerciseService;
 
     @PostMapping
-    public void addExercise(@RequestBody Exercise exercise) {
+    public void addExercise(@RequestBody @Valid Exercise exercise) {
         exerciseService.addExercise(exercise);
     }
 }
--- project/backend/src/main/java/com/exercise/project/entities/Exercise.java
+++ project/backend/src/main/java/com/exercise/project/entities/Exercise.java
@@ -2,6 +2,7 @@ package com.exercise.project.entities;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +18,7 @@ public class Exercise {
     @Column(name = "id")
     private Long id;
 
+    @NotBlank
     @Column(name = "name")
     private String name;
 
