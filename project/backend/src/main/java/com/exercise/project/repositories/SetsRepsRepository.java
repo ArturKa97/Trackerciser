fa96e46ffd50b980ff7afe0809ec6fd732ee427f
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-07-21 20:58:28 +0300

    Implementation on temporary basic structure for SetsReps entity

---
+++ project/backend/src/main/java/com/exercise/project/repositories/SetsRepsRepository.java
@@ -0,0 +1,7 @@
+package com.exercise.project.repositories;
+
+import com.exercise.project.entities.SetsReps;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SetsRepsRepository extends JpaRepository<SetsReps, Long> {
+}
