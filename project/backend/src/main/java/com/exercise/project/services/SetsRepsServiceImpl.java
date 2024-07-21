c7078653f4a42181206fba1ede414dfb5c96d52c
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-07-21 21:13:18 +0300

    Implementing SetsReps adding to DB.

--- project/backend/src/main/java/com/exercise/project/services/SetsRepsServiceImpl.java
+++ project/backend/src/main/java/com/exercise/project/services/SetsRepsServiceImpl.java
@@ -1,6 +1,7 @@
 package com.exercise.project.services;
 
 
+import com.exercise.project.entities.SetsReps;
 import com.exercise.project.repositories.SetsRepsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,9 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class SetsRepsServiceImpl implements SetsRepsService{
     private final SetsRepsRepository setsRepsRepository;
+
+    @Override
+    public void addSetsReps(SetsReps setsReps) {
+        setsRepsRepository.save(setsReps);
+    }
 }
