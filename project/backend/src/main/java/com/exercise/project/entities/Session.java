89b2a9dc58eec9efe10d1859e00f5b157a1467c3
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-07-21 20:48:04 +0300

    Addition of another entity responsible for the whole workout session  ,Session

---
+++ project/backend/src/main/java/com/exercise/project/entities/Session.java
@@ -0,0 +1,30 @@
+package com.exercise.project.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "session")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Session {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "date")
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private Date date;
+
+}
