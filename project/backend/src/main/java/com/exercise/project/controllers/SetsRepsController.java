c4cc82b4b6be8543e3724082237ff06c340cda1d
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-07-21 21:13:18 +0300

    Implementing SetsReps adding to DB.

--- project/backend/src/main/java/com/exercise/project/controllers/SetsRepsController.java
+++ project/backend/src/main/java/com/exercise/project/controllers/SetsRepsController.java
@@ -1,7 +1,10 @@
 package com.exercise.project.controllers;
 
+import com.exercise.project.entities.SetsReps;
 import com.exercise.project.services.SetsRepsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class SetsRepsController {
     private final SetsRepsService setsRepsService;
+
+    @PostMapping
+    public void addSetsReps (@RequestBody SetsReps setsReps) {
+        setsRepsService.addSetsReps(setsReps);
+    }
 }
--- project/backend/src/main/java/com/exercise/project/services/SetsRepsService.java
+++ project/backend/src/main/java/com/exercise/project/services/SetsRepsService.java
@@ -1,4 +1,7 @@
 package com.exercise.project.services;
 
+import com.exercise.project.entities.SetsReps;
+
 public interface SetsRepsService {
+    void addSetsReps(SetsReps setsReps);
 }
