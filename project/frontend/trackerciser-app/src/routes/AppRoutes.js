b82b55870ff4ff306f5aa5c6d29de4c140882bed
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-09-15 20:15:44 +0300

    Created a WorkoutSession component

--- project/frontend/trackerciser-app/src/routes/AppRoutes.js
+++ project/frontend/trackerciser-app/src/routes/AppRoutes.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import ExerciseTable from "../components/ExerciseTable";
 import WorkoutSessionsTable from "../components/WorkoutSessionsTable";
+import WorkoutSession from "../components/WorkoutSession";
 
 function AppRoutes() {
     return (
         <Routes>
             <Route path="/exercises" element={<ExerciseTable/>}/>
             <Route path="/workoutSessions" element={<WorkoutSessionsTable/>}/>
+            <Route path="/workoutSession" element={<WorkoutSession/>}/>
         </Routes>
       );
 }
