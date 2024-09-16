0f74480e1b62571e87aba7524b970bc93be00813
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-09-16 22:25:17 +0300

    Displaying the exercises for the selected workout session, the exercise information displayed is still wrong and will need to be worked on

--- project/frontend/trackerciser-app/src/components/ExerciseTable.js
+++ project/frontend/trackerciser-app/src/components/ExerciseTable.js
@@ -1,59 +1,53 @@
-import * as React from "react";
+import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import { retrieveAllExercises } from "../api/ExerciseApi";
-import { useState, useEffect } from "react";
+import * as React from "react";
 
-function ExerciseTable() {
-  const [exercises, setExercises] = useState([]);
-
-  useEffect(() => testCall(), []);
-
-  function testCall() {
-    retrieveAllExercises()
-      .then((response) => setExercises(response.data))
-      .catch((error) => console.log(error))
-      .finally(() => console.log("cleanup"));
-  }
+function ExerciseTable(props) {
+  const exercises = props.workoutSessionExercises;
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>ID</TableCell>
-            <TableCell align="center">Exercise name</TableCell>
-            <TableCell align="center">Sets</TableCell>
-            <TableCell align="center">Reps</TableCell>
-            <TableCell align="center">Weight</TableCell>
-            <TableCell align="center">Rest</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {exercises.map((exercise) => (
-            <TableRow
-              key={exercise.id}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {exercise.id}
-              </TableCell>
-              <TableCell align="center">{exercise.name}</TableCell>
-              {exercise.exerciseInfo.map((info) => (
-                <TableCell key={info.id} align="center">
-                  {info.sets}
-                </TableCell>
-              ))}
+    exercises && (
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell align="center">Exercise name</TableCell>
+              <TableCell align="center">Sets</TableCell>
+              <TableCell align="center">Reps</TableCell>
+              <TableCell align="center">Weight</TableCell>
+              <TableCell align="center">Rest</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {exercises.map((exercise) => (
+              <TableRow
+                key={exercise.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {exercise.id}
+                </TableCell>
+                <TableCell align="center">{exercise.name}</TableCell>
+                {exercise.exerciseInfo.map((info) => (
+                  <>
+                    <TableCell key={info.id} align="center">
+                      {info.sets}
+                    </TableCell>
+                    <TableCell align="center">{info.reps}</TableCell>
+                  </>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    )
   );
 }
 export default ExerciseTable;
--- project/frontend/trackerciser-app/src/routes/AppRoutes.js
+++ project/frontend/trackerciser-app/src/routes/AppRoutes.js
@@ -1,16 +1,16 @@
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import ExerciseTable from "../components/ExerciseTable";
+import WorkoutSessionPage from "../components/WorkoutSessionPage";
 import WorkoutSessionsTable from "../components/WorkoutSessionsTable";
-import WorkoutSession from "../components/WorkoutSession";
 
 function AppRoutes() {
-    return (
-        <Routes>
-            <Route path="/exercises" element={<ExerciseTable/>}/>
-            <Route path="/workoutSessions" element={<WorkoutSessionsTable/>}/>
-            <Route path="/workoutSession" element={<WorkoutSession/>}/>
-        </Routes>
-      );
+  return (
+    <Routes>
+      <Route path="/exercises" element={<ExerciseTable />} />
+      <Route path="/workoutSessions" element={<WorkoutSessionsTable />} />
+      <Route path="/workoutSession" element={<WorkoutSessionPage />} />
+    </Routes>
+  );
 }
 
 export default AppRoutes;
