5a8eae9da67b54a39ea7b85ae5cabcdb8b8a94db
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-09-21 20:05:56 +0300

    Refactoring REST methods for setsReps to have a specific workoutSession attached.

--- project/backend/src/main/java/com/exercise/project/services/SetsRepsServiceImpl.java
+++ project/backend/src/main/java/com/exercise/project/services/SetsRepsServiceImpl.java
@@ -3,8 +3,10 @@ package com.exercise.project.services;
 
 import com.exercise.project.entities.Exercise;
 import com.exercise.project.entities.SetsReps;
+import com.exercise.project.entities.WorkoutSession;
 import com.exercise.project.repositories.ExerciseRepository;
 import com.exercise.project.repositories.SetsRepsRepository;
+import com.exercise.project.repositories.WorkoutSessionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +15,13 @@ import org.springframework.stereotype.Service;
 public class SetsRepsServiceImpl implements SetsRepsService{
     private final SetsRepsRepository setsRepsRepository;
     private final ExerciseRepository exerciseRepository;
+    private final WorkoutSessionRepository workoutSessionRepository;
 
     @Override
-    public void addInfoToExercise(SetsReps setsReps, Long exerciseId) {
+    public void addInfoToExercise(SetsReps setsReps, Long workoutSessionId, Long exerciseId) {
+        WorkoutSession workoutSession = workoutSessionRepository.getWorkoutSessionById(workoutSessionId);
         Exercise exercise = exerciseRepository.getExerciseById(exerciseId);
+        workoutSession.addSetsReps(setsReps);
         exercise.addExerciseInfo(setsReps);
         setsRepsRepository.save(setsReps);
     }
