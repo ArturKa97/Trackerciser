d8d21c8132e8d9be34ba76bc8d1fd24838e50c36
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-10-08 20:37:44 +0300

    Implementing a basic form validation when adding a new workout session

--- project/frontend/trackerciser-app/src/forms/WorkoutSessionForm.js
+++ project/frontend/trackerciser-app/src/forms/WorkoutSessionForm.js
@@ -2,9 +2,9 @@ import { Field, Form, Formik } from "formik";
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
 import { addWorkoutSession } from "../api/WorkoutSessionApi";
+import { workoutSessionFormSchema } from "../schemas";
 
 function WorkoutSessionForm() {
-
   const navigate = useNavigate();
 
   const onSubmit = (values, actions) => {
@@ -21,13 +21,14 @@ function WorkoutSessionForm() {
       .catch((error) => console.log(error))
       .finally(() => console.log("cleanup"));
   };
-  
+
   return (
     <Formik
       initialValues={{ workoutSessionName: "asasa", date: "2022-02-02" }}
+      validationSchema={workoutSessionFormSchema}
       onSubmit={onSubmit}
     >
-      {({ isSubmitting }) => (
+      {({ isSubmitting, errors, touched }) => (
         <Form>
           <Field
             label="Name"
@@ -35,12 +36,16 @@ function WorkoutSessionForm() {
             name="workoutSessionName"
             type="text"
           />
+          {touched.workoutSessionName && errors.workoutSessionName && (
+            <div>{errors.workoutSessionName}</div>
+          )}
           <Field
             label="Date"
             placeholder="Enter your workout session date"
             name="date"
             type="date"
           />
+          {touched.date && errors.date && <div>{errors.date}</div>}
           <Button variant="outlined" type="submit" disabled={isSubmitting}>
             Submit
           </Button>
