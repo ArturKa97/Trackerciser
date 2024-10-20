023743bad3f7dcbf37b0c085e4926c10b1e3cb9a
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-10-20 19:26:23 +0300

    Created a simple form for adding exercise info and displaying it instead of exerciseTable, later it will render instead of exercise info values and not to a completely different component. Also implemented two different actions and buttons depending if we are adding or editing the current exercise info

---
+++ project/frontend/trackerciser-app/src/forms/SetsRepsForm.js
@@ -0,0 +1,54 @@
+import { Field, Form, Formik } from "formik";
+import Button from "@mui/material/Button";
+
+function SetsRepsForm({exerciseInfo, onFormClose, isAddingNew }) {
+  return (
+    <Formik
+      initialValues={{
+        sets: exerciseInfo?.sets || "",
+        reps: exerciseInfo?.reps || "",
+        weight: exerciseInfo?.weight || "",
+        rest: exerciseInfo?.rest || "",
+      }}
+      //   validationSchema={workoutSessionFormSchema}
+      onSubmit={(values) => {
+        if (isAddingNew) {
+          console.log("Adding new exerciseInfo:", values);
+        } else {
+          console.log("Editing exerciseInfo:", values);
+        }
+        onFormClose();
+      }}
+    >
+      {({ isSubmitting, errors, touched }) => (
+        <Form>
+          <Field label="Sets" placeholder="Set" name="sets" type="text" />
+          <Field
+            label="Reps"
+            placeholder="Repetitions"
+            name="reps"
+            type="text"
+          />
+          <Field
+            label="Weight"
+            placeholder="Weight"
+            name="weight"
+            type="text"
+          />
+          <Field
+            label="Rest"
+            placeholder="Rest between sets"
+            name="rest"
+            type="text"
+          />
+
+          <Button variant="outlined" type="submit" disabled={isSubmitting}>
+            {isAddingNew ? "Add" : "Update"}
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+export default SetsRepsForm;
