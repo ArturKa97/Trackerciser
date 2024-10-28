49964f44ea5f8a98745023805f449c634cf920ca
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-10-28 22:14:09 +0200

    Adding a simple form validation for adding Setsreps.

--- project/frontend/trackerciser-app/src/schemas/index.js
+++ project/frontend/trackerciser-app/src/schemas/index.js
@@ -7,3 +7,29 @@ export const workoutSessionFormSchema = yup.object().shape({
     .required("Required"),
   date: yup.date().required("Required")
 });
+
+export const setsRepsFormSchema = yup.object().shape({
+  sets: yup.number("Value must be a number")
+  .min(1, ({min}) => `Value cannot be less than ${min}` )
+  .max(999, ({max}) => `Maximum value is ${max}` )
+  .positive("Value must be positive")
+  .integer("Must be an integer")
+  .required("Required field"),
+   reps: yup.number("Value must be a number")
+  .min(1, ({min}) => `Value cannot be less than ${min}` )
+  .max(999, ({max}) => `Maximum value is ${max}` )
+  .positive("Value must be positive")
+  .integer("Must be an integer")
+  .required("Required field"),
+  weight: yup.number("Value must be a number")
+  .min(0.1, ({min}) => `Value cannot be less than ${min}` )
+  .max(9999, ({max}) => `Maximum value is ${max}` )
+  .positive("Value must be positive")
+  .required("Required field"),
+  rest: yup.number("Value must be a number")
+  .min(1, ({min}) => `Value cannot be less than ${min}` )
+  .max(999, ({max}) => `Maximum value is ${max}` )
+  .positive("Value must be positive")
+  .integer("Must be an integer")
+  .required("Required field")
+});
