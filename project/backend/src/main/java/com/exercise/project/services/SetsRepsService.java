b00979210d9d558243bb3b815d824162fbefb6e1
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-11-02 16:54:36 +0200

    Implementation of updating SetsReps info by ID.

--- project/backend/src/main/java/com/exercise/project/services/SetsRepsService.java
+++ project/backend/src/main/java/com/exercise/project/services/SetsRepsService.java
@@ -3,6 +3,7 @@ package com.exercise.project.services;
 import com.exercise.project.entities.SetsReps;
 
 public interface SetsRepsService {
-    SetsReps addInfoToExercise(SetsReps setsReps,Long exerciseId);
-    void removeInfoById (Long setsRepsId);
+    SetsReps addInfoToExercise(SetsReps setsReps, Long exerciseId);
+    void removeInfoById(Long setsRepsId);
+    SetsReps updateExerciseInfoById(SetsReps updatedSetsReps, Long setsRepsId);
 }
