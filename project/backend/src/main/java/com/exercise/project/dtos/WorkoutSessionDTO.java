b61f6acbf191c8e7949a8c795eddb5b1e88f9598
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-11-12 20:49:57 +0200

    Introducing tests for ExerciseServiceImpl addExerciseToWorkoutSession function to return an ExerciseDTO after  successfully adding and Exercise to WorkoutSession;

--- project/backend/src/main/java/com/exercise/project/dtos/WorkoutSessionDTO.java
+++ project/backend/src/main/java/com/exercise/project/dtos/WorkoutSessionDTO.java
@@ -2,11 +2,13 @@ package com.exercise.project.dtos;
 
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import lombok.Builder;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 import java.util.Set;
 
+@Builder
 public record WorkoutSessionDTO(
         Long id,
         String workoutSessionName,
