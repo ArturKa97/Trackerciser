e056e85cd79a8159853b9b2791ae4b3474309eec
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-11-12 19:17:25 +0200

    Introducing tests for ExerciseRepository getExerciseById function , to return an Exercise entity when it exists by given id , and return an Empty Optional when it doesn't exist.

--- project/backend/src/main/java/com/exercise/project/dtos/ExerciseDTO.java
+++ project/backend/src/main/java/com/exercise/project/dtos/ExerciseDTO.java
@@ -1,7 +1,10 @@
 package com.exercise.project.dtos;
 
+import lombok.Builder;
+
 import java.util.List;
 
+@Builder
 public record ExerciseDTO(
         Long id,
         ExerciseTypeDTO exerciseTypeDTO,
