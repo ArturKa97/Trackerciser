d3e880f6a4ff078ca6b6d34939d3aedf13d04366
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-11-12 21:45:54 +0200

    Introducing tests to WorkoutSessionServiceImpl getWorkoutSessionById function, to return a WorkoutSessionDTO if it exists and throw an EntityNotFoundException if no data is present.

--- project/backend/src/test/java/com/exercise/project/services/WorkoutSessionServiceImplTest.java
+++ project/backend/src/test/java/com/exercise/project/services/WorkoutSessionServiceImplTest.java
@@ -4,6 +4,7 @@ import com.exercise.project.dtos.WorkoutSessionDTO;
 import com.exercise.project.entities.WorkoutSession;
 import com.exercise.project.mappers.WorkoutSessionDTOMapper;
 import com.exercise.project.repositories.WorkoutSessionRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,9 +12,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Date;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -52,6 +53,43 @@ class WorkoutSessionServiceImplTest {
         verify(workoutSessionDTOMapper, times(1)).toEntity(workoutSessionDTO);
         verify(workoutSessionRepository, times(1)).save(workoutSession);
         verify(workoutSessionDTOMapper, times(1)).toDTO(workoutSession);
+    }
+
+    @Test
+    public void WorkoutSessionServiceImpl_GetWorkoutSessionById_ShouldReturnWorkoutSessionDTO() {
+        //Given
+        WorkoutSession workoutSession = WorkoutSession.builder()
+                .id(1L)
+                .workoutSessionName("Leg Day")
+                .date(new Date())
+                .build();
+        WorkoutSessionDTO workoutSessionDTO = WorkoutSessionDTO.builder()
+                .id(1L)
+                .workoutSessionName("Leg Day")
+                .date(new Date())
+                .build();
+        when(workoutSessionRepository.getWorkoutSessionById(workoutSession.getId())).thenReturn(Optional.of(workoutSession));
+        when(workoutSessionDTOMapper.toDTO(workoutSession)).thenReturn(workoutSessionDTO);
+        //When
+        WorkoutSessionDTO result = workoutSessionServiceImpl.getWorkoutSessionById(workoutSession.getId());
+        //Then
+        assertNotNull(result);
+        assertEquals(result, workoutSessionDTO);
+        verify(workoutSessionRepository, times(1)).getWorkoutSessionById(workoutSession.getId());
+        verify(workoutSessionDTOMapper, times(1)).toDTO(workoutSession);
+
+    }
+
+    @Test
+    public void WorkoutSessionServiceImpl_GetWorkoutSessionById_ShouldThrowEntityNotFoundException() {
+        //Given
+        Long nonExistentId = 999999L;
+        when(workoutSessionRepository.getWorkoutSessionById(nonExistentId)).thenReturn(Optional.empty());
+        //When + Then
+        EntityNotFoundException exception = assertThrows(EntityNotFoundException.class, () -> {
+            workoutSessionServiceImpl.getWorkoutSessionById(nonExistentId);
+        });
+        assertEquals("WorkoutSession with id [999999] not found", exception.getMessage());
     }
 
 }
