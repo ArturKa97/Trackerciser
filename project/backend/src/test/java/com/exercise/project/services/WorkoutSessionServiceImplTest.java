a4e82bc2f844774b38f73eea105c629a0ef71691
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-11-12 21:55:41 +0200

    Introducing tests to WorkoutSessionServiceImpl getAllWorkoutSessions function, to return a WorkoutSessionDTO List if it exists and return an empty List if no data is present.

--- project/backend/src/test/java/com/exercise/project/services/WorkoutSessionServiceImplTest.java
+++ project/backend/src/test/java/com/exercise/project/services/WorkoutSessionServiceImplTest.java
@@ -11,8 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Date;
-import java.util.Optional;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -90,6 +89,59 @@ class WorkoutSessionServiceImplTest {
             workoutSessionServiceImpl.getWorkoutSessionById(nonExistentId);
         });
         assertEquals("WorkoutSession with id [999999] not found", exception.getMessage());
+    }
+
+    @Test
+    public void WorkoutSessionServiceImpl_GetAllWorkoutSessions_ShouldReturnAListOfWorkoutSessionDTOS() {
+        //Given
+        WorkoutSession workoutSession1 = WorkoutSession.builder()
+                .id(1L)
+                .workoutSessionName("Leg Day")
+                .date(new Date())
+                .build();
+        WorkoutSession workoutSession2 = WorkoutSession.builder()
+                .id(2L)
+                .workoutSessionName("Chest Day")
+                .date(new Date())
+                .build();
+        List<WorkoutSession> workoutSessions = Arrays.asList(workoutSession1, workoutSession2);
+
+        WorkoutSessionDTO workoutSessionDTO1 = WorkoutSessionDTO.builder()
+                .id(1L)
+                .workoutSessionName("Leg Day")
+                .date(new Date())
+                .build();
+        WorkoutSessionDTO workoutSessionDTO2 = WorkoutSessionDTO.builder()
+                .id(2L)
+                .workoutSessionName("Chest Day")
+                .date(new Date())
+                .build();
+        List<WorkoutSessionDTO> workoutSessionsDTOs = Arrays.asList(workoutSessionDTO1, workoutSessionDTO2);
+
+        when(workoutSessionRepository.getAllWorkoutSessions()).thenReturn(workoutSessions);
+        when(workoutSessionDTOMapper.toDTO(workoutSession1)).thenReturn(workoutSessionDTO1);
+        when(workoutSessionDTOMapper.toDTO(workoutSession2)).thenReturn(workoutSessionDTO2);
+        //When
+        List<WorkoutSessionDTO> result = workoutSessionServiceImpl.getAllWorkoutSessions();
+        //Then
+        assertNotNull(result);
+        assertEquals(result, workoutSessionsDTOs);
+        verify(workoutSessionRepository, times(1)).getAllWorkoutSessions();
+        verify(workoutSessionDTOMapper, times(1)).toDTO(workoutSession1);
+        verify(workoutSessionDTOMapper, times(1)).toDTO(workoutSession2);
+    }
+
+    @Test
+    public void WorkoutSessionServiceImpl_GetAllWorkoutSessions_ShouldReturnAnEmptyList() {
+        //Given
+        when(workoutSessionRepository.getAllWorkoutSessions()).thenReturn(Collections.emptyList());
+        //When
+        List<WorkoutSessionDTO> result = workoutSessionServiceImpl.getAllWorkoutSessions();
+        //Then
+        assertNotNull(result);
+        assertTrue(result.isEmpty());
+        verify(workoutSessionRepository, times(1)).getAllWorkoutSessions();
+
     }
 
 }
