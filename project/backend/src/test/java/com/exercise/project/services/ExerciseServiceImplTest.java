f387e59fa2500e6eb74f465583b84c460e5dbe12
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-11-12 19:55:07 +0200

    Introducing tests for ExerciseServiceImpl getAllExercises function to return an ExerciseDTO List , or return an Empty List if no data is present.

--- project/backend/src/test/java/com/exercise/project/services/ExerciseServiceImplTest.java
+++ project/backend/src/test/java/com/exercise/project/services/ExerciseServiceImplTest.java
@@ -11,6 +11,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -60,6 +63,54 @@ class ExerciseServiceImplTest {
             exerciseServiceImpl.getExerciseById(nonExistentId);
         });
         assertEquals("Exercise with id [999999] not found", exception.getMessage());
+    }
+
+    @Test
+    public void ExerciseServiceImpl_GetAllExercises_ShouldReturnAnExerciseList() {
+        //Given
+        Exercise exercise1 = Exercise.builder()
+                .id(1L)
+                .build();
+
+        Exercise exercise2 = Exercise.builder()
+                .id(2L)
+                .build();
+
+        List<Exercise> exercises = Arrays.asList(exercise1, exercise2);
+
+        ExerciseDTO exerciseDTO1 = ExerciseDTO.builder()
+                .id(1L)
+                .build();
+
+        ExerciseDTO exerciseDTO2 = ExerciseDTO.builder()
+                .id(1L)
+                .build();
+
+        List<ExerciseDTO> exercisesDTOs = Arrays.asList(exerciseDTO1, exerciseDTO2);
+
+        when(exerciseRepository.getAllExercises()).thenReturn(exercises);
+        when(exerciseDTOMapper.toDTO(exercise1)).thenReturn(exerciseDTO1);
+        when(exerciseDTOMapper.toDTO(exercise2)).thenReturn(exerciseDTO2);
+        //When
+        List<ExerciseDTO> result = exerciseServiceImpl.getAllExercises();
+        //Then
+        assertNotNull(result);
+        assertEquals(result, exercisesDTOs);
+        verify(exerciseRepository, times(1)).getAllExercises();
+        verify(exerciseDTOMapper, times(1)).toDTO(exercise1);
+        verify(exerciseDTOMapper, times(1)).toDTO(exercise2);
+    }
+
+    @Test
+    public void ExerciseServiceImpl_GetAllExercises_ShouldReturnAnEmptyList() {
+        //Given
+        when(exerciseServiceImpl.getAllExercises()).thenReturn(Collections.emptyList());
+        //When
+        List<ExerciseDTO> result = exerciseServiceImpl.getAllExercises();
+        //Then
+        assertNotNull(result);
+        assertTrue(result.isEmpty());
+        verify(exerciseRepository, times(1)).getAllExercises();
     }
 }
 
