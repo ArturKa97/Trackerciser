167e734241597259780ae741cca196bc07185b5d
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-11-14 19:56:32 +0200

    Not using any or eq.

--- project/backend/src/test/java/com/exercise/project/controllers/ExerciseSetControllerTest.java
+++ project/backend/src/test/java/com/exercise/project/controllers/ExerciseSetControllerTest.java
@@ -14,8 +14,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -57,7 +55,7 @@ class ExerciseSetControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().json(objectMapper.writeValueAsString(exerciseSetDTO)));
 
-        verify(exerciseSetService).addExerciseSetToExercise(any(ExerciseSetDTO.class), eq(exerciseId));
+        verify(exerciseSetService).addExerciseSetToExercise(exerciseSetDTO, exerciseId);
     }
 
     @Test
