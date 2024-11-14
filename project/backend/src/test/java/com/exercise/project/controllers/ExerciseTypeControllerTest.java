432965227082f29799938088ccd6aadff1570ef2
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-11-14 19:51:48 +0200

    Introducing ExerciseTypeController tests for getAllExerciseTypes function call.

---
+++ project/backend/src/test/java/com/exercise/project/controllers/ExerciseTypeControllerTest.java
@@ -0,0 +1,67 @@
+package com.exercise.project.controllers;
+
+import com.exercise.project.dtos.ExerciseTypeDTO;
+import com.exercise.project.services.ExerciseTypeService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = ExerciseTypeController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+class ExerciseTypeControllerTest {
+
+    @MockBean
+    private ExerciseTypeService exerciseTypeService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void ExerciseTypeController_GetAllExerciseTypes() throws Exception {
+        //Given
+        ExerciseTypeDTO exerciseTypeDTO1 = ExerciseTypeDTO.builder()
+                .id(1L)
+                .name("Squats")
+                .build();
+
+        ExerciseTypeDTO exerciseTypeDTO2 = ExerciseTypeDTO.builder()
+                .id(1L)
+                .name("Curls")
+                .build();
+
+        List<ExerciseTypeDTO> exerciseTypeDTOList = Arrays.asList(exerciseTypeDTO1, exerciseTypeDTO2);
+
+        when(exerciseTypeService.getAllExerciseTypes()).thenReturn(exerciseTypeDTOList);
+        //When
+        ResultActions response = mockMvc.perform(get("/exercise_type")
+                .contentType(MediaType.APPLICATION_JSON));
+        //Then
+        response
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(exerciseTypeDTOList)));
+
+        verify(exerciseTypeService).getAllExerciseTypes();
+    }
+
+}
