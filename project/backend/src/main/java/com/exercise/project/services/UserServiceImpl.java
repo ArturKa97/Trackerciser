aef052d35fc42dbcef05423db4c96f6adf6814b4
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-11-18 21:01:08 +0200

    Adding a USER role to the newly added user, and implementing a simple getUserById method, for checking if the correct info is being received.

--- project/backend/src/main/java/com/exercise/project/services/UserServiceImpl.java
+++ project/backend/src/main/java/com/exercise/project/services/UserServiceImpl.java
@@ -1,22 +1,54 @@
 package com.exercise.project.services;
 
+import com.exercise.project.entities.Role;
 import com.exercise.project.entities.User;
+import com.exercise.project.entities.UserRole;
+import com.exercise.project.entities.UserRoleId;
+import com.exercise.project.repositories.RoleRepository;
 import com.exercise.project.repositories.UserRepository;
+import com.exercise.project.repositories.UserRoleRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-    public void addNewUser(User user) {
+    private final RoleRepository roleRepository;
+    private final UserRoleRepository userRoleRepository;
+
+    public void addNewUser(User userToRegister) {
         User newUser = User.builder()
-                .username(user.getUsername())
-                .password(passwordEncoder.encode(user.getPassword()))
+                .username(userToRegister.getUsername())
+                .password(passwordEncoder.encode(userToRegister.getPassword()))
                 .build();
-        userRepository.save(newUser);
+        User savedUser = userRepository.save(newUser);
+
+        Role role = roleRepository.findById("USER")
+                .orElseThrow(() -> new EntityNotFoundException("[%s] role not found".formatted("USER")));
+
+        UserRoleId userRoleId = UserRoleId.builder()
+                .userId(savedUser.getId())
+                .roleId(role.getRole())
+                .build();
+
+        UserRole userRole = UserRole.builder()
+                .userRoleId(userRoleId)
+                .user(savedUser)
+                .role(role)
+                .build();
+
+        userRoleRepository.save(userRole);
+    }
+
+    @Override
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
     }
 }
