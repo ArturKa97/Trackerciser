070be7f863e27775286bbbd6c6de4d16a99fd0d1
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-11-18 21:01:08 +0200

    Adding a USER role to the newly added user, and implementing a simple getUserById method, for checking if the correct info is being received.

--- project/backend/src/main/java/com/exercise/project/controllers/UserController.java
+++ project/backend/src/main/java/com/exercise/project/controllers/UserController.java
@@ -3,10 +3,9 @@ package com.exercise.project.controllers;
 import com.exercise.project.entities.User;
 import com.exercise.project.services.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -18,6 +17,10 @@ public class UserController {
     @PostMapping
     public void addNewUser(@RequestBody User user) {
         userService.addNewUser(user);
+    }
+    @GetMapping("/{id}")
+    public Optional<User> getUserById (@PathVariable (value = "id") Long id) {
+        return userService.getUserById(id);
     }
 
 }
--- project/backend/src/main/java/com/exercise/project/services/UserService.java
+++ project/backend/src/main/java/com/exercise/project/services/UserService.java
@@ -2,8 +2,11 @@ package com.exercise.project.services;
 
 import com.exercise.project.entities.User;
 
+import java.util.Optional;
+
 public interface UserService {
 
     void addNewUser(User user);
 
+    Optional<User> getUserById(Long id);
 }
