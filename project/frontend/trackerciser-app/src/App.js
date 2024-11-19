39e98dde787022110a0783bc6ef65471c4df4a3a
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-11-19 20:47:34 +0200

    Adding a userSlice to the Redux store and wraping the project with the store Provider.

--- project/frontend/trackerciser-app/src/App.js
+++ project/frontend/trackerciser-app/src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import AppRoutes from "./routes/AppRoutes";
+import store from "./store/store";
 
 function App() {
   return (
-    <BrowserRouter>     
-        <Header/>
-        <AppRoutes/> 
+    <BrowserRouter>
+      <Provider store={store}>
+        <Header />
+        <AppRoutes />
+      </Provider>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
