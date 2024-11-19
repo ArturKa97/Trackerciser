eb842f8b9742dccc30790347a8b08d360f9e0771
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-11-19 19:02:38 +0200

    Adding a Login component, with its own button and route.

---
+++ project/frontend/trackerciser-app/src/forms/LoginForm.js
@@ -0,0 +1,40 @@
+import { Field, Form, Formik } from "formik";
+import Button from "@mui/material/Button";
+
+function LoginForm() {
+  //   const onSubmit
+
+  return (
+    <Formik
+      //   validationSchema={workoutSessionFormSchema}
+      //   onSubmit={onSubmit}
+      enableReinitialize
+    >
+      {({ isSubmitting, errors, touched }) => (
+        <Form>
+          <Field
+            label="Username"
+            placeholder="Username"
+            name="usename"
+            type="text"
+          />
+          {/* {touched.workoutSessionName && errors.workoutSessionName && (
+            <div>{errors.workoutSessionName}</div>
+          )} */}
+          <Field
+            label="Password"
+            placeholder="Password"
+            name="password"
+            type="password"
+          />
+          {/* {touched.date && errors.date && <div>{errors.date}</div>} */}
+          <Button variant="outlined" type="submit" disabled={isSubmitting}>
+            Login
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+export default LoginForm;
