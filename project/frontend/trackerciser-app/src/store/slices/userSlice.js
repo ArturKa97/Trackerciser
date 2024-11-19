c8120d1d6073ac3561af62eb4316d658e32e53db
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-11-19 20:36:59 +0200

    Adding userSlice for storing jwt token and later setting it to null after logging out. Dispatching logged in user info(jwt token) to the redux store.

---
+++ project/frontend/trackerciser-app/src/store/slices/userSlice.js
@@ -0,0 +1,24 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = null
+
+const userSlice = createSlice({
+        name: "userSlice",
+        initialState,
+        reducers: {
+            userLoggedIn(userState, {payload: loggedInUser}) {
+                return loggedInUser;
+            },
+            userLoggedOut(){
+                return initialState
+            }
+
+        }
+    }
+)
+export const selectLoggedInUser = state => state.userSlice?.token
+
+export default userSlice.reducer;
+
+export const { userLoggedIn, userLoggedOut } = userSlice.actions
+
