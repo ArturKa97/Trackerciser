974c43d6ddf9ef4552e4f648e381de9e3776f79a
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-11-19 20:14:28 +0200

    Refactoring /authenticate call to only return data.

--- project/frontend/trackerciser-app/src/api/UserApi.js
+++ project/frontend/trackerciser-app/src/api/UserApi.js
@@ -1,10 +1,9 @@
 import HTTP from "./";
 
 export function registerNewUser(user) {
-    return HTTP.post("/user", user);
-  }
+  return HTTP.post("/user", user);
+}
 
-  export function loginAndAuthenticate(user) {
-    return HTTP.post("/authenticate", user);
-  }
-  
+export function loginAndAuthenticate(user) {
+  return HTTP.post("/authenticate", user).then(({ data }) => data);
+}
