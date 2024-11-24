a4465c4a3f8e7051cc5fb6b0c1174c24b57b0bd0
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-11-24 20:09:43 +0200

    Introducing tests for UserRepository getUserById call to return a User when it exists and return an empty optional when no data is present. Refactoring a reserved "user" table name in H2 with back ticks so the tests won't fail.

---
+++ project/backend/src/test/java/com/exercise/project/repositories/UserRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.exercise.project.repositories;
+
+import com.exercise.project.entities.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+@TestPropertySource(properties = "spring.sql.init.mode=never")
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void UserRepository_GetUserById_ShouldReturnUserEntity() {
+        //Given
+        User user = User.builder()
+                .username("user")
+                .password("user")
+                .build();
+        //When
+        User savedUser = userRepository.save(user);
+        User fetchedUser = userRepository.getUserById(savedUser.getId()).get();
+
+        //Then
+        assertThat(fetchedUser).isNotNull()
+                .isEqualTo(savedUser);
+
+    }
+
+    @Test
+    public void UserRepository_GetUserById_ShouldReturnOptionalEmptyWhenUserDoesNotExist() {
+        //Given
+        Long nonExistentId = 999999L;
+        //When
+        Optional<User> fetchedUser = userRepository.getUserById(nonExistentId);
+        //Then
+        assertThat(fetchedUser).isNotNull()
+                .isEmpty();
+    }
+}
+
