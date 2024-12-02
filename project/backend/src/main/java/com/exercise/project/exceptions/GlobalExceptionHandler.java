926492fe57bf4b8f13a5d575adb98c1c7ca966a6
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-12-02 20:08:19 +0200

    Deleting my "custom" exception class since it did not do anything, was just an experiment.

--- project/backend/src/main/java/com/exercise/project/exceptions/GlobalExceptionHandler.java
+++ project/backend/src/main/java/com/exercise/project/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.exercise.project.exceptions;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
