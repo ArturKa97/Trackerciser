81155166a98427bc444b7a29c7f923de886e92c2
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-12-02 21:43:58 +0200

    Adding global exception handling for IllegalArgumentException, MethodArgumentNotValidException, ConstraintViolationException and default unhandled exceptions that throw 500 "Internal Server Error".

--- project/backend/src/main/java/com/exercise/project/exceptions/CustomErrorResponse.java
+++ project/backend/src/main/java/com/exercise/project/exceptions/CustomErrorResponse.java
@@ -8,6 +8,7 @@ import org.springframework.web.ErrorResponse;
 
 import java.net.URI;
 import java.util.Date;
+import java.util.List;
 
 @Builder
 @Data
@@ -16,14 +17,16 @@ public class CustomErrorResponse implements ErrorResponse {
     private String type;
     private String title;
     private HttpStatusCode statusCode;
-    private String message;
+    private String detail;
+    private List<String> details;
     private Date timeStamp;
 
     @Override
     public ProblemDetail getBody() {
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(statusCode, message);
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(statusCode, detail);
         problemDetail.setTitle(title);
         problemDetail.setType(URI.create(type));
+        problemDetail.setProperty("details", details);
         problemDetail.setProperty("timestamp", timeStamp);
         return problemDetail;
     }
