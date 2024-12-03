72bbb3ed6198d3f2c9e8f1d2abe446a8b34e2d92
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-12-03 20:20:43 +0200

    Adding validations for User and Role entities.

--- project/backend/src/main/java/com/exercise/project/entities/Role.java
+++ project/backend/src/main/java/com/exercise/project/entities/Role.java
@@ -2,6 +2,9 @@ package com.exercise.project.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -27,6 +30,9 @@ public class Role implements GrantedAuthority {
     private Long id;
 
     @Column(name = "role")
+    @NotNull(message = "Role value cannot be null")
+    @NotBlank(message = "Role value cannot be empty")
+    @Size(min = 1, max = 30, message = "Role value must be between {min} and {max} characters long")
     private String role;
 
     @OneToMany(mappedBy = "role",
--- project/backend/src/main/java/com/exercise/project/entities/User.java
+++ project/backend/src/main/java/com/exercise/project/entities/User.java
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -30,9 +33,15 @@ public class User implements UserDetails {
     private Long id;
 
     @Column(name = "username")
+    @NotNull(message = "Username value cannot be null")
+    @NotBlank(message = "Username value cannot be empty")
+    @Size(min = 1, max = 20, message = "Username value must be between {min} and {max} characters long")
+    //Might add a regex expression
     private String username;
 
     @Column(name = "password")
+    @NotNull(message = "Password value cannot be null")
+    @NotBlank(message = "Password value cannot be empty")
     @Pattern(
             regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
             message = "Password must be at least 8 characters long, and include at least one uppercase letter, one lowercase letter, one number, and one special character."
