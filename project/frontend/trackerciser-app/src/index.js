8efc03668167d8f375dd7c6c76d134542c1a9709
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-12-03 22:49:36 +0200

    Creating a theme and starting to style some of the frontend components.

--- project/frontend/trackerciser-app/src/index.js
+++ project/frontend/trackerciser-app/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import AppTheme from './styles/AppTheme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={AppTheme}>
+    <CssBaseline />
     <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
