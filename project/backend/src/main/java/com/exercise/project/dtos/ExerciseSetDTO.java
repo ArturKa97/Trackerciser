2fed6786fced4243b60c1e557b779300f1e02025
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2024-12-03 20:00:52 +0200

    Adding more validations for ExerciseSet entity, and changing Float to BigDecimal, so the DB would recognize a decimal value after adding a @Digits annotation

--- project/backend/src/main/java/com/exercise/project/dtos/ExerciseSetDTO.java
+++ project/backend/src/main/java/com/exercise/project/dtos/ExerciseSetDTO.java
@@ -2,12 +2,14 @@ package com.exercise.project.dtos;
 
 import lombok.Builder;
 
+import java.math.BigDecimal;
+
 @Builder
 public record ExerciseSetDTO(
         Long id,
         Long sets,
         Long reps,
-        Float weight,
+        BigDecimal weight,
         Long rest) {
 
 }
