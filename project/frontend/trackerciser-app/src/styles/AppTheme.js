d4d8023cd75e6119f9199732fa38f660364c4109
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2025-01-31 23:08:51 +0200

    Separating logo into its own box and styling the header with it.

--- project/frontend/trackerciser-app/src/styles/AppTheme.js
+++ project/frontend/trackerciser-app/src/styles/AppTheme.js
@@ -1,6 +1,23 @@
 import { createTheme } from "@mui/material";
 import background from "../img/background.jpg";
 
+/*
+--- TYPOGRAPHY SYSTEM
+
+- Font sizes (px)
+10 / 12 / 14 / 16 / 18 / 20 / 24 / 30 / 36 / 44 / 52 / 62 / 74 / 86 / 98
+
+- Font weights
+Default: 400
+Medium: 500
+Semi-bold: 600
+Bold: 700
+---  WHITESPACE
+
+- Spacing system (px)
+2 / 4 / 8 / 12 / 16 / 24 / 32 / 48 / 64 / 80 / 96 / 128
+*/
+
 const AppTheme = createTheme({
   palette: {
     primary: {
@@ -24,8 +41,9 @@ const AppTheme = createTheme({
           height: "100vh",
           backgroundPosition: "center",
         },
+      },
     },
   },
-}});
+});
 
 export default AppTheme;
