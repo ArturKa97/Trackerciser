6b3b8db16804420fbd696e2f974c9c9417466b07
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2025-02-02 20:32:19 +0200

    Minor styling changes and adding a start of a carousel component that is going to switch between Cards with all the features on them.

--- project/frontend/trackerciser-app/src/components/HomePage.js
+++ project/frontend/trackerciser-app/src/components/HomePage.js
@@ -1,54 +1,40 @@
-import { Paper, Box, Typography } from "@mui/material";
-import { MainContainer, TwoColumnGridBox } from "../styles/StyledComponents";
+import { Box, Typography } from "@mui/material";
+import {
+  AlignStartGridBox,
+  FeaturesCard,
+  FeaturesCardContent,
+  FeaturesTypography,
+  MainContainer,
+  TwoColumnGridBox,
+} from "../styles/StyledComponents";
 
 function HomePage() {
   return (
     <MainContainer>
       <TwoColumnGridBox>
         <Box>
-          <Typography variant="h1">EASY WAY TO TRACK YOUR WORKOUTS</Typography>
+          <Typography variant="h1">
+            SIMPLIFY YOUR WORKOUT AND PROGRESS TRACKING
+          </Typography>
         </Box>
-        <Box>
-          <Paper>
-            <Typography>Tracking Workouts</Typography>
-            <Typography>
-              Ability to track your workouts, just simply create a workout
-              session with a name and date of your choice, select the exercises
-              that you did on that specific day and input your data on how many
-              sets, repetitions you did with what weight and how much rest you
-              had in between sets or exercises, also you can add a time for how
-              long you did the specific set, usefull for the static exercises
-              that just need to be held for time in a fixed position. Then you
-              always have an ability to add, delete, or edit anything included.
-            </Typography>
-          </Paper>
-          <Paper>
-            <Typography>Progress visualization</Typography>
-            <Typography>
-              Tracking your progress by going through each workout session one
-              by one and just looking at the specific exercise and specific
-              numbers would take lots of time and effort. Thats why you have an
-              ability to visually see your desired progress by simply selecting
-              the "Charts" section, then selecting your desired date interval
-              and finally you have an ability to select any exercise you did in
-              the specified time period and switch between all the trackable
-              data "Sets, Reps, Weight, Rest, Duration" and see the progress
-              visually represented by a line chart, which shows the difference
-              between your workouts on each date in the selected period.
-            </Typography>
-          </Paper>
-          <Paper>
-            <Typography>Setting goals</Typography>
-            <Typography>
-              Future goals: In the future the ability to set individual goals
-              for each user will be implemented. Whether it would be a goal
-              related to workouts or something along the lines of losing weight.
-              This will probably be integrated with "Charts", to see how far or
-              how close you are to your set goals, and maybe if you managed to
-              beat them in the time that you desired.
-            </Typography>
-          </Paper>
-        </Box>
+        <AlignStartGridBox>
+          <FeaturesCard>
+            <FeaturesCardContent>
+              <Typography variant="h2">TRACKING WORKOUTS</Typography>
+              <FeaturesTypography>
+                Ability to track your workouts, just simply create a workout
+                session with a name and date of your choice, select the
+                exercises that you did on that specific day and input your data
+                on how many sets, repetitions you did with what weight and how
+                much rest you had in between sets or exercises, also you can add
+                a time for how long you did the specific set, usefull for the
+                static exercises that just need to be held for time in a fixed
+                position. Then you always have an ability to add, delete, or
+                edit anything included.
+              </FeaturesTypography>
+            </FeaturesCardContent>
+          </FeaturesCard>
+        </AlignStartGridBox>
       </TwoColumnGridBox>
     </MainContainer>
   );
