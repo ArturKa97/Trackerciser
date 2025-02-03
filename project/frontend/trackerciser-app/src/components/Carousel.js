9b6bbac9e97a8f73df835b068f244668cc7aaf90
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2025-02-03 22:41:52 +0200

    Introducing the Carousel of FeatureCards that you can switch between with button clicks and a smooth animation.

---
+++ project/frontend/trackerciser-app/src/components/Carousel.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { Typography } from "@mui/material";
+import {
+  AlignStartGridBox,
+  FeaturesCard,
+  FeaturesCardContent,
+  FeaturesList,
+  FeaturesListItem,
+  NextIconButton,
+  PrevIconButton,
+} from "../styles/StyledComponents";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
+function Carousel() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [direction, setDirection] = useState(1); // 1 for next, -1 for prev
+
+  function handlePreviousCardClick() {
+    setDirection(-1);
+    setActiveIndex(
+      (prevIndex) => (prevIndex - 1 + cardData.length) % cardData.length
+    );
+    console.log(activeIndex);
+  }
+
+  function handleNextCardClick() {
+    setDirection(1);
+    setActiveIndex((prevIndex) => (prevIndex + 1) % cardData.length);
+    console.log(activeIndex);
+  }
+
+  const cardData = [
+    {
+      title: "TRACKING WORKOUTS",
+      content: [
+        "Create a workout session with a custom name and date",
+        "Select exercises performed on that day.",
+        "Input workout details: Sets, Reps, Weight, Rest and Duration",
+        "Modify your workout history anytime, add, edit, or delete entries as needed.",
+      ],
+    },
+    {
+      title: "VISUAL PROGRESS",
+      content: [
+        "Avoid manually reviewing individual workout sessions and visually track your progress instead",
+        "Access the Charts section to analyze progress over time.",
+        "Select the date range along with the specific exercise and switch between different metrics",
+        "View improvements through line charts, making it easy to analyze your training",
+      ],
+    },
+    {
+      title: "SETTING GOALS",
+      content: [
+        "Future update will allow users to set personal fitness goals",
+        "Workout specific targets along with body composition goals",
+        "Will be integrated with Charts for visual representation",
+        "Ability to compare actual perfomance with the set goals",
+      ],
+    },
+  ];
+
+  return (
+    <AlignStartGridBox>
+      <FeaturesCard>
+        <PrevIconButton
+          aria-label="previous"
+          onClick={() => handlePreviousCardClick()}
+        >
+          <ArrowBackIosNewIcon />
+        </PrevIconButton>
+        <FeaturesCardContent
+          direction={direction}
+          key={activeIndex}
+          className={`slide-in ${direction === 1 ? "next" : "prev"}`}
+        >
+          <Typography variant="h2">{cardData[activeIndex].title}</Typography>
+          <FeaturesList>
+            {cardData[activeIndex].content.map((feature, index) => (
+              <FeaturesListItem key={index}>{feature}</FeaturesListItem>
+            ))}
+          </FeaturesList>
+        </FeaturesCardContent>
+        <NextIconButton aria-label="next" onClick={() => handleNextCardClick()}>
+          <ArrowForwardIosIcon />
+        </NextIconButton>
+      </FeaturesCard>
+    </AlignStartGridBox>
+  );
+}
+
+export default Carousel;
--- project/frontend/trackerciser-app/src/components/HomePage.js
+++ project/frontend/trackerciser-app/src/components/HomePage.js
@@ -1,12 +1,6 @@
 import { Box, Typography } from "@mui/material";
-import {
-  AlignStartGridBox,
-  FeaturesCard,
-  FeaturesCardContent,
-  FeaturesTypography,
-  MainContainer,
-  TwoColumnGridBox,
-} from "../styles/StyledComponents";
+import { MainContainer, TwoColumnGridBox } from "../styles/StyledComponents";
+import Carousel from "./Carousel";
 
 function HomePage() {
   return (
@@ -17,24 +11,7 @@ function HomePage() {
             SIMPLIFY YOUR WORKOUT AND PROGRESS TRACKING
           </Typography>
         </Box>
-        <AlignStartGridBox>
-          <FeaturesCard>
-            <FeaturesCardContent>
-              <Typography variant="h2">TRACKING WORKOUTS</Typography>
-              <FeaturesTypography>
-                Ability to track your workouts, just simply create a workout
-                session with a name and date of your choice, select the
-                exercises that you did on that specific day and input your data
-                on how many sets, repetitions you did with what weight and how
-                much rest you had in between sets or exercises, also you can add
-                a time for how long you did the specific set, usefull for the
-                static exercises that just need to be held for time in a fixed
-                position. Then you always have an ability to add, delete, or
-                edit anything included.
-              </FeaturesTypography>
-            </FeaturesCardContent>
-          </FeaturesCard>
-        </AlignStartGridBox>
+        <Carousel></Carousel>
       </TwoColumnGridBox>
     </MainContainer>
   );
