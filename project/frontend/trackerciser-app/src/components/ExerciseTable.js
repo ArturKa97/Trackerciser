79d4c67e2466d97813513246f11f0a1e30f5d9dc
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2025-02-12 21:37:09 +0200

    Simplifying the ExerciseTable component to not have double mapping just to have the name on the first row, instead using the index now. Also changing buttons to fit with the rest of the theme

--- project/frontend/trackerciser-app/src/components/ExerciseTable.js
+++ project/frontend/trackerciser-app/src/components/ExerciseTable.js
@@ -5,12 +5,18 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import * as React from "react";
 import { useState } from "react";
-import { Button } from "@mui/material";
 import ExerciseSetForm from "../forms/ExerciseSetForm";
 import { removeExerciseSetById } from "../api/ExerciseSetApi";
 import { removeExerciseFromWorkoutSession } from "../api/ExerciseApi";
 import ClearIcon from "@mui/icons-material/Clear";
-import { DeleteOrCloseActionButton } from "../styles/StyledComponents";
+import EditIcon from "@mui/icons-material/Edit";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
+import {
+  AddActionButton,
+  DeleteOrCloseActionButton,
+  EditActionButton,
+} from "../styles/StyledComponents";
 
 function ExerciseTable({
   workoutSessionExercises,
@@ -76,143 +82,85 @@ function ExerciseTable({
             const exerciseSets = exercise.exerciseSetsDTO || [];
             return (
               <React.Fragment key={exercise.id}>
-                <TableRow>
-                  <TableCell>
-                    <DeleteOrCloseActionButton
-                      aria-label="delete"
-                      onClick={() =>
-                        removeExerciseFromWorkoutSessionCall(
-                          workoutSessionId,
-                          exercise.id
-                        )
-                      }
-                    >
-                      <ClearIcon />
-                    </DeleteOrCloseActionButton>
-                  </TableCell>
-                  <TableCell>{exercise.exerciseTypeDTO.name}</TableCell>
-                  {exerciseSets.length > 0 &&
-                  editingExerciseId === exercise.id &&
-                  editingExerciseSetId === exerciseSets[0].id ? (
-                    <>
-                      <TableCell colSpan={7}>
+                {exerciseSets.map((set, index) => (
+                  <TableRow key={set.id}>
+                    {index === 0 ? (
+                      <>
+                        <TableCell>
+                          <DeleteOrCloseActionButton
+                            aria-label="delete"
+                            onClick={() =>
+                              removeExerciseFromWorkoutSessionCall(
+                                workoutSessionId,
+                                exercise.id
+                              )
+                            }
+                          >
+                            <ClearIcon />
+                          </DeleteOrCloseActionButton>
+                        </TableCell>
+                        <TableCell>{exercise.exerciseTypeDTO.name}</TableCell>
+                      </>
+                    ) : (
+                      <>
+                        <TableCell />
+                        <TableCell />
+                      </>
+                    )}
+                    {exerciseSets.length > 0 &&
+                    editingExerciseId === exercise.id &&
+                    editingExerciseSetId === set.id ? (
+                      <TableCell colSpan={8}>
                         <ExerciseSetForm
-                          exerciseSets={exerciseSets[0]}
+                          exerciseSets={set}
                           onFormClose={handleFormClose}
                           isAddingNew={false}
                         />
                       </TableCell>
-                    </>
-                  ) : (
-                    <>
-                      <TableCell align="center">
-                        {exerciseSets[0]?.sets}
-                      </TableCell>
-                      <TableCell align="center">
-                        {exerciseSets[0]?.reps}
-                      </TableCell>
-                      <TableCell align="center">
-                        {exerciseSets[0]?.weight}
-                      </TableCell>
-                      <TableCell align="center">
-                        {exerciseSets[0]?.rest}
-                      </TableCell>
-                      <TableCell align="center">
-                        {exerciseSets.length > 0 && (
-                          <>
-                            <Button
-                              onClick={() =>
-                                handleEditClick(exercise.id, exerciseSets[0].id)
-                              }
-                              variant="outlined"
-                            >
-                              Edit
-                            </Button>
-                            <Button
-                              variant="outlined"
-                              onClick={() =>
-                                removeExerciseSetByIdCall(exerciseSets[0].id)
-                              }
-                            >
-                              Delete
-                            </Button>
-                          </>
-                        )}
-                      </TableCell>
-                    </>
-                  )}
-                </TableRow>
-
-                {exerciseSets.length > 1 &&
-                  exerciseSets.slice(1)?.map((set) => (
-                    <TableRow key={set.id}>
-                      {editingExerciseId === exercise.id &&
-                      editingExerciseSetId === set.id ? (
-                        <>
-                          <TableCell colSpan={7}>
-                            <ExerciseSetForm
-                              exerciseSets={set}
-                              onFormClose={handleFormClose}
-                              isAddingNew={false}
-                            />
-                          </TableCell>
-                        </>
-                      ) : (
-                        <>
-                          <TableCell align="center" />
-                          <TableCell align="center" />
-                          <TableCell align="center">{set.sets}</TableCell>
-                          <TableCell align="center">{set.reps}</TableCell>
-                          <TableCell align="center">{set.weight}</TableCell>
-                          <TableCell align="center">{set.rest}</TableCell>
-                          <TableCell align="center">
-                            <Button
-                              onClick={() =>
-                                handleEditClick(exercise.id, set.id)
-                              }
-                              variant="outlined"
-                            >
-                              Edit
-                            </Button>
-                            <Button
-                              variant="outlined"
-                              onClick={() => removeExerciseSetByIdCall(set.id)}
-                            >
-                              Delete
-                            </Button>
-                          </TableCell>
-                        </>
-                      )}
-                    </TableRow>
-                  ))}
+                    ) : (
+                      <>
+                        <TableCell align="center">{set.sets}</TableCell>
+                        <TableCell align="center">{set.reps}</TableCell>
+                        <TableCell align="center">{set.weight}</TableCell>
+                        <TableCell align="center">{set.rest}</TableCell>
+                        <TableCell align="center">
+                          <EditActionButton
+                            onClick={() => handleEditClick(exercise.id, set.id)}
+                          >
+                            <EditIcon />
+                          </EditActionButton>
+                          <DeleteOrCloseActionButton
+                            onClick={() => removeExerciseSetByIdCall(set.id)}
+                          >
+                            <DeleteIcon />
+                          </DeleteOrCloseActionButton>
+                        </TableCell>
+                      </>
+                    )}
+                  </TableRow>
+                ))}
                 {addingNewExercise && editingExerciseId === exercise.id ? (
-                  <>
-                    <TableRow>
-                      <TableCell colSpan={7}>
-                        <ExerciseSetForm
-                          exerciseId={exercise.id}
-                          exerciseSets={null}
-                          onFormClose={handleFormClose}
-                          isAddingNew={true}
-                        />
-                      </TableCell>
-                    </TableRow>
-                  </>
+                  <TableRow>
+                    <TableCell colSpan={7}>
+                      <ExerciseSetForm
+                        exerciseId={exercise.id}
+                        exerciseSets={null}
+                        onFormClose={handleFormClose}
+                        isAddingNew={true}
+                      />
+                    </TableCell>
+                  </TableRow>
                 ) : (
-                  <>
-                    <TableRow>
-                      <TableCell colSpan={7}>
-                        <Button
-                          onClick={() => handleAddClick(exercise.id)}
-                          variant="contained"
-                          color="primary"
-                          disabled={!!editingExerciseId}
-                        >
-                          Add Set
-                        </Button>
-                      </TableCell>
-                    </TableRow>
-                  </>
+                  <TableRow>
+                    <TableCell colSpan={8}>
+                      <AddActionButton
+                        onClick={() => handleAddClick(exercise.id)}
+                        disabled={!!editingExerciseId}
+                      >
+                        <AddIcon />
+                      </AddActionButton>
+                    </TableCell>
+                  </TableRow>
                 )}
               </React.Fragment>
             );
