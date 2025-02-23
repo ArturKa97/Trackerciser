1e107518e4c9819b1748108ff9e412ce40a77f72
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2025-02-23 12:44:39 +0200

    Implementing the pagination on getAllWorkoutSessions function in the backend.

--- project/backend/src/main/java/com/exercise/project/repositories/WorkoutSessionRepository.java
+++ project/backend/src/main/java/com/exercise/project/repositories/WorkoutSessionRepository.java
@@ -1,6 +1,8 @@
 package com.exercise.project.repositories;
 
 import com.exercise.project.entities.WorkoutSession;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,8 +16,8 @@ public interface WorkoutSessionRepository extends JpaRepository<WorkoutSession, 
     @Query(value = "SELECT ws FROM WorkoutSession ws LEFT JOIN FETCH ws.exercisesSet e LEFT JOIN FETCH e.exerciseType LEFT JOIN FETCH e.exerciseSets WHERE ws.id = :id")
     Optional<WorkoutSession> getWorkoutSessionById(@Param("id") Long id);
 
-    @Query(value = "SELECT ws FROM WorkoutSession ws LEFT JOIN FETCH ws.exercisesSet e LEFT JOIN FETCH e.exerciseType LEFT JOIN FETCH e.exerciseSets")
-    List<WorkoutSession> getAllWorkoutSessions();
+    @Query(value = "SELECT DISTINCT ws FROM WorkoutSession ws LEFT JOIN FETCH ws.exercisesSet e LEFT JOIN FETCH e.exerciseType LEFT JOIN FETCH e.exerciseSets")
+    Page<WorkoutSession> getAllWorkoutSessions(Pageable pageable);
 
     @Query(value = "SELECT ws FROM WorkoutSession ws LEFT JOIN FETCH ws.exercisesSet e LEFT JOIN FETCH e.exerciseType LEFT JOIN FETCH e.exerciseSets  WHERE ws.date BETWEEN:fromDate AND:toDate")
     List<WorkoutSession> getWorkoutSessionsBetweenDates(@Param("fromDate") LocalDate fromDate,
