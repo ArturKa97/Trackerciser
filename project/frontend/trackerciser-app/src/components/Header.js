9c2e0b2b33365dfdcb7164886ba4f4d22f131069
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2025-02-25 19:48:57 +0200

    Added a separate header menu for it appears on screens smaller than 754px.

--- project/frontend/trackerciser-app/src/components/Header.js
+++ project/frontend/trackerciser-app/src/components/Header.js
@@ -1,5 +1,7 @@
 import * as React from "react";
+import { useState } from "react";
 import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import MenuIcon from "@mui/icons-material/Menu";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectLoggedInUser, userLoggedOut } from "../store/slices/userSlice";
@@ -10,52 +12,134 @@ import {
   HeaderButtonBox,
   HeaderHomeButton,
   HeaderLogoBox,
+  HeaderMenuBoxSm,
+  HeaderMenuButton,
   LogoTypography,
   StyledAppBar,
 } from "../styles/StyledComponents";
+import { Box, Menu, MenuItem, useMediaQuery } from "@mui/material";
 
 function Header() {
   const user = useSelector(selectLoggedInUser);
   const dispatch = useDispatch();
+  const [anchor, setAnchor] = useState(null);
+  const open = !!anchor;
+  const isSm = useMediaQuery("(max-width:754px)");
+
+  const handleOpen = (event) => {
+    setAnchor(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchor(null);
+  };
 
   const navigate = useNavigate();
 
   return (
     <StyledAppBar>
       <FlexToolbar>
-        <HeaderButtonBox>
-          <HeaderLogoBox>
-            <HeaderHomeButton onClick={() => navigate("/")}>
-              <FitnessCenterIcon />
-              <LogoTypography>TRACKERCISER</LogoTypography>
-            </HeaderHomeButton>
-          </HeaderLogoBox>
-          <HeaderButton onClick={() => navigate("/workoutSessions")}>
-            Workout Sessions
-          </HeaderButton>
-          <HeaderButton onClick={() => navigate("/chart")}>Charts</HeaderButton>
-        </HeaderButtonBox>
-        <HeaderButtonBox>
-          {!user ? (
-            <>
-              <HeaderButton onClick={() => navigate("/login")}>
-                Login
+        {!isSm ? (
+          <>
+            <HeaderButtonBox>
+              <HeaderLogoBox>
+                <HeaderHomeButton onClick={() => navigate("/")}>
+                  <FitnessCenterIcon />
+                  <LogoTypography>TRACKERCISER</LogoTypography>
+                </HeaderHomeButton>
+              </HeaderLogoBox>
+              <HeaderButton onClick={() => navigate("/workoutSessions")}>
+                WORKOUT SESSIONS
               </HeaderButton>
-              <HeaderButton onClick={() => navigate("/register")}>
-                Register
+              <HeaderButton onClick={() => navigate("/chart")}>
+                CHARTS
               </HeaderButton>
-            </>
-          ) : (
-            <HeaderButton
-              onClick={() => {
-                dispatch(userLoggedOut());
-                navigate("/");
-              }}
-            >
-              Log Out
-            </HeaderButton>
-          )}
-        </HeaderButtonBox>
+            </HeaderButtonBox>
+            <HeaderButtonBox>
+              {!user ? (
+                <>
+                  <HeaderButton onClick={() => navigate("/login")}>
+                    LOGIN
+                  </HeaderButton>
+                  <HeaderButton onClick={() => navigate("/register")}>
+                    REGISTER
+                  </HeaderButton>
+                </>
+              ) : (
+                <HeaderButton
+                  onClick={() => {
+                    dispatch(userLoggedOut());
+                    navigate("/");
+                  }}
+                >
+                  LOG OUT
+                </HeaderButton>
+              )}
+            </HeaderButtonBox>
+          </>
+        ) : (
+          <HeaderMenuBoxSm>
+            <HeaderLogoBox>
+              <HeaderHomeButton onClick={() => navigate("/")}>
+                <FitnessCenterIcon />
+                <LogoTypography>TRACKERCISER</LogoTypography>
+              </HeaderHomeButton>
+            </HeaderLogoBox>
+            {/* Render only at 754px and below*/}
+            <HeaderMenuButton onClick={handleOpen}>
+              <MenuIcon />
+            </HeaderMenuButton>
+            <Menu open={open} anchorEl={anchor} onClose={handleClose}>
+              <MenuItem
+                onClick={() => {
+                  navigate("/workoutSessions");
+                  handleClose();
+                }}
+              >
+                WORKOUT SESSIONS
+              </MenuItem>
+              <MenuItem
+                onClick={() => {
+                  navigate("/chart");
+                  handleClose();
+                }}
+              >
+                CHARTS
+              </MenuItem>
+              {!user ? (
+                <Box>
+                  <MenuItem
+                    key="login"
+                    onClick={() => {
+                      navigate("/login");
+                      handleClose();
+                    }}
+                  >
+                    LOG IN
+                  </MenuItem>
+                  <MenuItem
+                    key="register"
+                    onClick={() => {
+                      navigate("/register");
+                      handleClose();
+                    }}
+                  >
+                    REGISTER
+                  </MenuItem>
+                </Box>
+              ) : (
+                <MenuItem
+                  onClick={() => {
+                    dispatch(userLoggedOut());
+                    navigate("/");
+                    handleClose();
+                  }}
+                >
+                  LOG OUT
+                </MenuItem>
+              )}
+            </Menu>
+          </HeaderMenuBoxSm>
+        )}
       </FlexToolbar>
     </StyledAppBar>
   );
