30a9aa739016b9c9f00d62b0dba52b9ad709ab13
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2025-02-27 21:21:21 +0200

    Frontend portion of the jwt token commit, instead of storing the access token in local storage which is not secure, we store it memory, and send it with each request, after it expires we get an error with response and if its 401 we store the original request and try to fetch a new access token to the /refresh endpoint, if it still valid, we set the new access token to the memory and retry the original request, if not we log the user out and make him login again which starts the whole sequence again.

--- project/frontend/trackerciser-app/src/api/UserApi.js
+++ project/frontend/trackerciser-app/src/api/UserApi.js
@@ -1,9 +1,11 @@
 import HTTP from "./";
 
 export function registerNewUser(user) {
-  return HTTP.post("/user", user);
+  return HTTP.post("/user/register", user);
 }
 
 export function loginAndAuthenticate(user) {
-  return HTTP.post("/authenticate", user).then(({ data }) => data);
+  return HTTP.post("/authenticate", user).then((response) => {
+    return response.data;
+  });
 }
--- project/frontend/trackerciser-app/src/api/index.js
+++ project/frontend/trackerciser-app/src/api/index.js
@@ -1,24 +1,48 @@
 import axios from "axios";
+import { userLoggedIn, userLoggedOut } from "../store/slices/userSlice";
 
 const HTTP = axios.create({
   baseURL: "http://localhost:8080",
+  withCredentials: true,
 });
 
-//TODO: Implement the token removal after token expirationDate ends.
 const initBackendApiClient = (store) => {
   HTTP.interceptors.request.use((config) => {
-    let jwt = store.getState().userSlice?.token;
-
-    if (!jwt) {
-      jwt = localStorage.getItem("token");
+    if (config.url !== "/refresh") {
+      const jwt = store.getState().userSlice?.token;
+      if (jwt) {
+        config.headers.Authorization = "Bearer " + jwt;
+      }
     }
-
-    if (jwt) {
-      config.headers.Authorization = "Bearer " + jwt;
-    }
-
     return config;
   });
+
+  HTTP.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      const originalRequest = error.config;
+      if (!originalRequest) {
+        return Promise.reject(error);
+      }
+      if (error.response?.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true;
+        try {
+          const { data } = await HTTP.post(
+            "/refresh",
+            {},
+            { withCredentials: true }
+          );
+          store.dispatch(userLoggedIn({ token: data.token }));
+          originalRequest.headers.Authorization = `Bearer ${data.token}`;
+          return HTTP.request(originalRequest);
+        } catch (refreshError) {
+          store.dispatch(userLoggedOut());
+          return Promise.reject(refreshError);
+        }
+      }
+      return Promise.reject(error);
+    }
+  );
 };
 export default HTTP;
 export { initBackendApiClient };
