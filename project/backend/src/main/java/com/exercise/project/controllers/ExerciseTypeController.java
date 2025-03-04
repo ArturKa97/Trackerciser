4143fd80cdd1b87fe646c0c47e0ca6e9379bd44b
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2025-03-04 20:20:12 +0200

    Configuring ExerciseTypeController, changing the tag, and adding description's and summaries for all endpoints, for more clarification and easier understanding in the Swagger UI.

--- project/backend/src/main/java/com/exercise/project/controllers/ExerciseTypeController.java
+++ project/backend/src/main/java/com/exercise/project/controllers/ExerciseTypeController.java
@@ -2,6 +2,8 @@ package com.exercise.project.controllers;
 
 import com.exercise.project.dtos.ExerciseTypeDTO;
 import com.exercise.project.services.ExerciseTypeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +14,13 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/exercise_type")
+@Tag(name = "Exercise Types Access")
 public class ExerciseTypeController {
 
     private final ExerciseTypeService exerciseTypeService;
 
+    @Operation(description = "Get endpoint for receiving all Exercise Types",
+    summary = "Fetches a list of all available exercise types for a user choose from. This is a part of exercise creation")
     @GetMapping
     public List<ExerciseTypeDTO> getAllExerciseTypes() {
         return exerciseTypeService.getAllExerciseTypes();
