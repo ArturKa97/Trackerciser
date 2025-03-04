3660d0405115521b567813b773ef1b02cdfdeabc
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2025-03-04 20:39:09 +0200

    Configuring UserController, changing the tag, and adding description's and summaries for all endpoints, for more clarification and easier understanding in the Swagger UI.

--- project/backend/src/main/java/com/exercise/project/controllers/UserController.java
+++ project/backend/src/main/java/com/exercise/project/controllers/UserController.java
@@ -5,6 +5,8 @@ import com.exercise.project.entities.User;
 import com.exercise.project.services.RoleService;
 import com.exercise.project.services.RoleServiceImpl;
 import com.exercise.project.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -12,21 +14,28 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
+@Tag(name = "User's Registration And Role Managing")
 public class UserController {
 
     private final UserService userService;
     private final RoleService roleService;
 
+    @Operation(description = "Post endpoint for the new User registration",
+    summary = "Register the new user")
     @PostMapping("/register")
     public void addNewUser(@Valid @RequestBody User user) {
         userService.addNewUser(user);
     }
 
+    @Operation(description = "Get endpoint for retrieving the User",
+    summary = "Used for internal operations for user fetching, and for testing in the development")
     @GetMapping("/{id}")
     public UserDTO getUserById(@PathVariable(value = "id") Long id) {
         return userService.getUserById(id);
     }
 
+    @Operation(description = "Post endpoint for User's Role assignment",
+    summary = "Manual role assignment for user, will be hidden further in the development when ADMIN role will be required for some operations")
     @PostMapping("/{userId}/{roleId}")
     public void addRoleToUser(@PathVariable(value = "userId") Long userId, @PathVariable(value = "roleId") Long roleId) {
         roleService.addRoleToUser(userId, roleId);
