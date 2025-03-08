e7cf924e30e8c5b6950f4c514204205ca38470b7
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2025-03-08 20:56:33 +0200

    Adding more documentation for OpenApi Swagger UI for ExerciseTypeController including possible list of ApiResponses and their related information, also  more clarity and description for path, query params and request bodies.

--- project/backend/src/main/java/com/exercise/project/controllers/ExerciseTypeController.java
+++ project/backend/src/main/java/com/exercise/project/controllers/ExerciseTypeController.java
@@ -3,6 +3,11 @@ package com.exercise.project.controllers;
 import com.exercise.project.dtos.ExerciseTypeDTO;
 import com.exercise.project.services.ExerciseTypeService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +25,13 @@ public class ExerciseTypeController {
     private final ExerciseTypeService exerciseTypeService;
 
     @Operation(description = "Get endpoint for receiving all Exercise Types",
-    summary = "Fetches a list of all available exercise types for a user choose from. This is a part of exercise creation")
+            summary = "Fetches a list of all available exercise types for a user choose from. This is a part of exercise creation")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully returns the list of all exercise types",
+                    content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = ExerciseTypeDTO.class)))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized, JWT token invalid or expired", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Bad request, default response code for when something bad unexpected happens", content = @Content)
+    })
     @GetMapping
     public List<ExerciseTypeDTO> getAllExerciseTypes() {
         return exerciseTypeService.getAllExerciseTypes();
